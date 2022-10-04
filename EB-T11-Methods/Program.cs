903ba69538313fed3a98771fedfb1399a13497e8
fatihmaras <fatihmmaras@gmail.com>
2022-10-04 03:52:53 +0300

    Method Example

---
+++ EB-T11-Methods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodlaraGiris
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Selamver();
+            AdiniYaz();
+            Console.ReadLine();
+        }
+
+        static void Selamver()
+        {
+            Console.WriteLine("Selamlar:)");
+
+        }
+        static void AdiniYaz()
+        {
+            Console.WriteLine("Fatih MARAŞ");
+
+        }
+
+    }
+}
